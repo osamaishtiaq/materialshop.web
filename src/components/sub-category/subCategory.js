2501d9416e3400610a15cc4fd79ee3ed297fb8b4
hooriahic <hooria.hic@gmail.com>
2020-11-07 19:30:11 +0500

    fixed some issues

--- src/components/sub-category/subCategory.js
+++ src/components/sub-category/subCategory.js
@@ -15,11 +15,14 @@ function SubCategory({data}) {
     let { catId } = useParams();
     let fetchedData = data.find(x => x.id === catId);
     const [,setState] = useState();
+    const [isLoading, setIsLoading] = useState(false);
 
     const getSubCategory = (e) => {
         GetSubCategories(catId).then(newData => {
             SubData = newData;
+            //setIsLoading(true);
             setState({});
+   
         }).catch((err) => {
             console.info("Error in fetching sub categories");
             console.log(err);
@@ -31,39 +34,52 @@ function SubCategory({data}) {
     return (
         <div>
             <h1>{fetchedData.name}</h1>
-            <div className={classes.root}>
+                <div className={classes.root}>
                 {SubData.map((item, index) => {
-                    if(item.subcategory === false) {
-                        return (
-                            <div key={index + item.id} className={classes.card}>
-                                <img className={classes.cardImg} src={item.image} alt={item.name} />
-                                <div className={classes.cardRight}>
+                    if(isLoading === false){
+                        if(item.subcategory === false) {
+                            return (
+                                <div key={index + item.id} className={classes.card}>
+                                    <img className={classes.cardImg} src={item.image} alt={item.name} />
+                                    <div className={classes.cardRight}>
+                                        <p className={classes.cardText}>{item.name}</p>
+                                        <Button variant="contained" color="secondary">
+                                            <Link className={classes.cardLink} to="/productDetail">Shop All</Link>
+                                        </Button>
+                                    </div>
+                                </div>
+                            );
+                        }
+                        else if(item.subcategory === true){
+                            return (
+                                <div key={index + item.id} className={classes.card}>
+                                    <img className={classes.cardImg} src={item.image} alt={item.name} />
+                                    <div className={classes.cardRight}>
                                     <p className={classes.cardText}>{item.name}</p>
-                                    <Button variant="contained" color="secondary">
-                                        <Link className={classes.cardLink} to="/productDetail">Shop All</Link>
-                                    </Button>
-                                </div>
-                            </div>
-                        );
+                                        <Button variant="contained" color="secondary">Explore More</Button>
+                                    </div>
+                                </div> 
+                            );
+                        }
+                        else{
+                            return (
+                                <CircularProgress />
+                            );
+                        }
                     }
-                    else if(item.subcategory === true){
+                    else if(isLoading === true){
                         return (
-                            <div key={index + item.id} className={classes.card}>
-                                <img className={classes.cardImg} src={item.image} alt={item.name} />
-                                <div className={classes.cardRight}>
-                                <p className={classes.cardText}>{item.name}</p>
-                                    <Button variant="contained" color="secondary">Explore More</Button>
-                                </div>
-                            </div> 
-                        );
+                            <CircularProgress />
+                        )
                     }
                     else{
                         return (
-                            <CircularProgress />
-                        );
+                            <h1>Something went wrong</h1>
+                        )
                     }
                 })}
             </div>
+            
         </div>
     );
 }
